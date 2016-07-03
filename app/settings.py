13c87138ae8610878ada8cc89ef2416cc511ee97
larry sigo <sigocode@gmail.com>
2016-07-03 17:23:45 +0100

    updated setting for production

--- app/settings.py
+++ app/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -129,3 +129,5 @@ import dj_database_url
 
 DATABASES[‘default’] = dj_database_url.config()
 
+TEMPLATE_DEBUG = True
+SECURE_PROXY_SSL_HEADER = (‘HTTP_X_FORWARDED_PROTO’, ‘https’)
