036a3d1e4db83e2d71b6963620f9a8c3fb16780e
larry sigo <sigocode@gmail.com>
2016-07-04 01:28:46 +0100

    updated the settings_production

--- app/settings_production.py
+++ app/settings_production.py
@@ -9,7 +9,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
 DEBUG = False
 TEMPLATE_DEBUG = True
 #for database
-DATABASE['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 # for serving satic files in a production server
 STATICFILES_DIRS = ( os.path.join(BASE_DIR,'static'), )
 STATIC_ROOT = 'staticfiles'
